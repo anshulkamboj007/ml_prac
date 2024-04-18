ca436efb4275a738512993ce71f0a3c4bffdfa24
anshulkamboj007 <anshulsinghkamboj@gmail.com>
2024-04-18 16:29:35 +0530

    o4

---
+++ student/packaging-ml-model/prediction_model/config/config.py
@@ -0,0 +1,83 @@
+import pathlib
+import os
+import prediction_model
+
+PACKAGE_ROOT=pathlib.Path(prediction_model.__file__).resolve().parent
+print(PACKAGE_ROOT)
+
+DATAPATH = os.path.join(PACKAGE_ROOT,"datasets")
+TRAIN_FILE='train.csv'
+TEST_FILE='test.csv'
+
+SAVE_MODEL_PATH=os.path.join(PACKAGE_ROOT,'trained_models')
+
+TARGET = 'Target'
+
+FEATURES= ['Marital status', 'Application mode', 'Application order', 'Course',
+       'Daytime/evening attendance\t', 'Previous qualification',
+       'Previous qualification (grade)', 'Nacionality',
+       "Mother's qualification", "Father's qualification",
+       "Mother's occupation", "Father's occupation", 'Admission grade',
+       'Displaced', 'Educational special needs', 'Debtor',
+       'Tuition fees up to date', 'Gender', 'Scholarship holder',
+       'International',
+       'Curricular units 1st sem (credited)',
+       'Curricular units 1st sem (enrolled)',
+       'Curricular units 1st sem (evaluations)',
+       'Curricular units 1st sem (approved)',
+       'Curricular units 1st sem (grade)',
+       'Curricular units 1st sem (without evaluations)',
+       'Curricular units 2nd sem (credited)',
+       'Curricular units 2nd sem (enrolled)',
+       'Curricular units 2nd sem (evaluations)',
+       'Curricular units 2nd sem (approved)',
+       'Curricular units 2nd sem (grade)',
+       'Curricular units 2nd sem (without evaluations)', 'Unemployment rate',
+       'Inflation rate', 'GDP'
+       ]
+
+NUM_FEATURES = ['Age at enrollment','Unemployment rate','Inflation rate', 'GDP']
+
+CAT_FEATURES = ['Marital status', 'Application mode', 'Application order', 'Course',
+       'Daytime/evening attendance\t', 'Previous qualification',
+       'Previous qualification (grade)', 'Nacionality',
+       "Mother's qualification", "Father's qualification",
+       "Mother's occupation", "Father's occupation", 'Admission grade',
+       'Displaced', 'Educational special needs', 'Debtor',
+       'Tuition fees up to date', 'Gender', 'Scholarship holder',
+        'International',
+       'Curricular units 1st sem (credited)',
+       'Curricular units 1st sem (enrolled)',
+       'Curricular units 1st sem (evaluations)',
+       'Curricular units 1st sem (approved)',
+       'Curricular units 1st sem (grade)',
+       'Curricular units 1st sem (without evaluations)',
+       'Curricular units 2nd sem (credited)',
+       'Curricular units 2nd sem (enrolled)',
+       'Curricular units 2nd sem (evaluations)',
+       'Curricular units 2nd sem (approved)',
+       'Curricular units 2nd sem (grade)',
+       'Curricular units 2nd sem (without evaluations)'
+       ]
+
+FEATURES_TO_ENCODE = ['Marital status', 'Application mode', 'Application order', 'Course',
+       'Daytime/evening attendance\t', 'Previous qualification',
+       'Previous qualification (grade)', 'Nacionality',
+       "Mother's qualification", "Father's qualification",
+       "Mother's occupation", "Father's occupation", 'Admission grade',
+       'Displaced', 'Educational special needs', 'Debtor',
+       'Tuition fees up to date', 'Gender', 'Scholarship holder',
+        'International',
+       'Curricular units 1st sem (credited)',
+       'Curricular units 1st sem (enrolled)',
+       'Curricular units 1st sem (evaluations)',
+       'Curricular units 1st sem (approved)',
+       'Curricular units 1st sem (grade)',
+       'Curricular units 1st sem (without evaluations)',
+       'Curricular units 2nd sem (credited)',
+       'Curricular units 2nd sem (enrolled)',
+       'Curricular units 2nd sem (evaluations)',
+       'Curricular units 2nd sem (approved)',
+       'Curricular units 2nd sem (grade)',
+       'Curricular units 2nd sem (without evaluations)'
+       ]
