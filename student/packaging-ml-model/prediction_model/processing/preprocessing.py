a98b920b34c8f19daa353b9257ef06aca73f4b9c
anshul <anshulsinghkamboj@gmail.com>
2024-04-20 06:20:12 +0530

    ki1

--- student/packaging-ml-model/prediction_model/processing/preprocessing.py
+++ student/packaging-ml-model/prediction_model/processing/preprocessing.py
@@ -18,3 +18,18 @@ class MeanImputer(BaseEstimator,TransformerMixin):
         for col in self.variables:
             X[col].fillna(self.mean_dict[col],inplace=True)
         return X
+    
+
+class DropColumns(BaseEstimator,TransformerMixin):
+    def __init__(self,variables_to_drop=None):
+        self.variables_to_drop=variables_to_drop
+
+    def fit(self,X,y=None):
+
+        return self
+    
+    def transform(self,X):
+        X=X.copy()
+        X=X.drop(columns = self.variables_to_drop)
+
+        return X
